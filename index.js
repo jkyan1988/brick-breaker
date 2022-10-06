f2c7e6597a6283f39c9a49cbe79be6abfd6b8caf
James Kyan <james.kyan88@gmail.com>
2022-10-05 22:19:40 -0400

    second commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const grid = document.querySelector('.grid')
+const blockWidth = 100
+const blockHeight = 20
+
+//create Block
+class Block {
+    constructor(xAxis, yAxis) {
+        this.bottomLeft = [xAxis, yAxis]
+        this.bottomRight = [xAxis + blockWidth, yAxis]
+    }
+}
+
+//draw my block
+function addBlock() {
+    const block = document.createElement('div')
+    block.classList.add('block')
+    block.style.left = '100px'
+    block.style.bottom = '50px'
+    grid.appendChild(block)
+}
+
+addBlock()
